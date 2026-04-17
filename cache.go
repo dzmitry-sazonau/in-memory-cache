561efff273d33da6e68c33f5f95445381393dece
Dzmitry Sazonau <dzmitry.sazonau@gmail.com>
2026-04-17 11:44:34 +0200

    added ttl for cache and lazy checker

--- cache.go
+++ cache.go
@@ -1,27 +1,60 @@
 package cache
 
+import (
+	"errors"
+	"sync"
+	"time"
+)
+
 type Cache struct {
-	cache map[string]interface{}
+	mu    *sync.Mutex
+	cache map[string]Item
+}
+
+type Item struct {
+	value any
+	ttl   time.Time
 }
 
 func New() *Cache {
-	return &Cache{make(map[string]interface{})}
+	return &Cache{
+		new(sync.Mutex),
+		make(map[string]Item),
+	}
 }
 
-func (c *Cache) Set(key string, value interface{}) {
-	c.cache[key] = value
+func (c *Cache) Set(key string, value any, ttl time.Duration) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c.cache[key] = Item{value, time.Now().Add(ttl)}
 }
 
-func (c *Cache) Get(key string) interface{} {
-	var value, ok = c.cache[key]
+func (c *Cache) Get(key string) (any, error) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	item, ok := c.cache[key]
 
 	if !ok {
-		return "unknown key"
+		return nil, errors.New("key not found")
 	}
 
-	return value
+	if time.Now().After(item.ttl) {
+		c.delete(key)
+		return nil, errors.New("key is expired")
+	}
+
+	return item.value, nil
 }
 
 func (c *Cache) Delete(key string) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	delete(c.cache, key)
+}
+
+func (c *Cache) delete(key string) {
 	delete(c.cache, key)
 }
